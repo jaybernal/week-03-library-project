985d95a7587b29ee74e1a40bfc2fb7e50b21b745
Jay Bernal <jjbernal@gmail.com>
2016-03-31 10:52:08 -0500

    completed library model and test

--- tests/library_test.rb
+++ tests/library_test.rb
@@ -6,9 +6,29 @@ class LibraryTest < Minitest::Test
 	def test_invalid_without_branch_name
 	l = Library.new(branch_name: "Star City",
 									address: "151 N 8th Street\, Suite 500\, Lincoln\, NE 68508",
-									phone_number: 402-555-9876)
+									phone_number: 4025559876)
 	assert(l.valid?, "Should be valid at initialization")
 	l.branch_name =  nil
 	refute(l.valid?, "Should be invalid without a branch name")
 	end
+
+def test_invalid_without_branch_address
+	l = Library.new(branch_name: "Star City",
+									address: "151 N 8th Street\, Suite 500\, Lincoln\, NE 68508",
+									phone_number: 4025559876)
+	assert(l.valid?, "Should be valid at initialization")
+	l.address =  nil
+	refute(l.valid?, "Should be invalid without a branch address")
+	end
+
+	def test_invalid_without_branch_phone_number
+	l = Library.new(branch_name: "Star City",
+									address: "151 N 8th Street\, Suite 500\, Lincoln\, NE 68508",
+									phone_number: 4025559876)
+	assert(l.valid?, "Should be valid at initialization")
+	l.phone_number =  nil
+	refute(l.valid?, "Should be invalid without a branch phone number")
+	end
+
+
 end
